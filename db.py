73bf3610a0259b8f1ace98892905a03b46e4f3e1
Splinterfeel <skkiminock@mail.ru>
2024-10-01 21:59:40 +0300

    change Sqlite3 mock to Postgres with asyncpg

--- db.py
+++ db.py
@@ -1,47 +1,45 @@
-import sqlite3
-import os
-from pathlib import Path
-
-DB_PATH = Path(os.getcwd(), 'main.db')
-
-def init() -> None:
-    if not os.path.isfile(DB_PATH):
-        create_new_db()
+import asyncpg
 
 
-def create_new_db():
-    print('init new db')
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE users
+DB_PARAMS = {
+    'database': 'postgres',
+    'user': 'pguser',
+    'password': 'pgpassword',
+    'host': 'db',
+    'port': 5432,
+}
+
+
+async def init() -> None:
+    conn: asyncpg.connection = await asyncpg.connect(**DB_PARAMS)
+    transaction = conn.transaction()
+    await transaction.start()
+    await conn.execute('''CREATE TABLE IF NOT EXISTS tg_users
                    (
                     username VARCHAR(400) NOT NULL,
                     client_id VARCHAR(400) NOT NULL,
                     client_secret VARCHAR(400) NOT NULL,
                     UNIQUE(username, client_id, client_secret)
-                   )''')
-    conn.commit()
-    conn.close()
+                    );''')
+    await transaction.commit()
+    await conn.close()
 
 
-def add_client_info(username, client_id, client_secret):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('''INSERT INTO users (username, client_id, client_secret)
-                   VALUES (?, ?, ?)''',
-                   [username, str(client_id), str(client_secret)])
-    conn.commit()
-    conn.close()
+async def add_client_info(username, client_id, client_secret):
+    conn: asyncpg.connection = await asyncpg.connect(**DB_PARAMS)
+    transaction = conn.transaction()
+    await transaction.start()
+    await conn.execute('''INSERT INTO tg_users (username, client_id, client_secret)
+                       VALUES ($1, $2, $3)''',
+                       username, str(client_id), str(client_secret))
+    await transaction.commit()
+    await conn.close()
 
 
-def get_client_info(username):
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users WHERE username = ?', [username])
-    columns = [x[0] for x in cursor.description]
-    result = cursor.fetchall()
-    result = [dict(zip(columns, x)) for x in result]
-    conn.close()
-    if result:
-        return result[0]
+async def get_client_info(username):
+    conn: asyncpg.connection = await asyncpg.connect(**DB_PARAMS)
+    values = await conn.fetch('SELECT * FROM tg_users WHERE username = $1', username)
+    await conn.close()
+    if values:
+        return values[0]
     return None
